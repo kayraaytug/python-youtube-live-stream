db0e02fa50e1248d55af44dc13dea643f0fbfc56
kayraaytug <kayraaytug@outlook.com>
2023-02-23 15:07:25 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+# import required libraries
+from vidgear.gears import CamGear
+from vidgear.gears import WriteGear, ScreenGear
+import cv2
+
+# define and open video source
+stream = CamGear().start()
+
+# define required FFmpeg parameters for your writer
+output_params = {
+    "-clones": ["-f", "lavfi", "-i", "anullsrc"],
+    "-vcodec": "libx264",
+    "-preset": "medium",
+    "-b:v": "4500k",
+    "-bufsize": "512k",
+    "-pix_fmt": "yuv420p",
+    "-f": "flv",
+}
+
+#Change your YouTube-Live Stream Key here:
+YOUTUBE_STREAM_KEY = "Youtube-Stream-Key"
+
+# Define writer with defined parameters
+writer = WriteGear(
+    output="rtmp://a.rtmp.youtube.com/live2/{}".format(YOUTUBE_STREAM_KEY),
+    logging=True,
+    **output_params
+)
+
+# loop over
+while True:
+
+    # read frames from stream
+    frame = stream.read()
+
+    # check for frame if Nonetype
+    if frame is None:
+        break
+
+    # {do something with the frame here}
+
+    # write frame to writer
+    writer.write(frame)
+
+# safely close video stream
+stream.stop()
+
+# safely close writer
+writer.close()
